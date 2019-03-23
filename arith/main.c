5665dea58a88c3e079414a5590a5778d996d0ed3
strawbs98 <vaidushi98@gmail.com>
2019-03-23 16:37:38 +0530

    first commit

---
+++ arith/main.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int add(int, int);
+int sub(int, int);
+
+int main(int argc, char const *argv[])
+{
+    printf("Add: %d\n", add(10,20));
+    printf("Sub: %d\n", sub(30,50));
+    return 0;
+}
